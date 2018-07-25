ef364e41f774b38e590b2845051e5e4f97716e4f
Richard Chien <richardchienthebest@gmail.com>
2018-07-25 22:51:06 +0800

    Update base config

--- config_base.py
+++ config_base.py
@@ -2,9 +2,10 @@ from none.default_config import *
 
 COMMAND_START.add('')
 NICKNAME = {'小丸子', 'Maruko'}
+DATA_FOLDER = os.path.join(os.path.dirname(__file__), 'data')
 
 DATABASE_URL = ''
-SESSION_CANCELLATION_EXPRESSION = ('好的', '明白')
+SESSION_CANCELLATION_EXPRESSION = ('好的', '明白了')
 
 BAIDU_AIP_APP_ID = ''
 BAIDU_AIP_API_KEY = ''
