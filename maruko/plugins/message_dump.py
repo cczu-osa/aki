6f7434ab44411897a39e5642211e76fddbc051d5
Richard Chien <richardchienthebest@gmail.com>
2018-07-29 23:59:11 +0800

    Improve message dump plugin

--- maruko/plugins/message_dump.py
+++ maruko/plugins/message_dump.py
@@ -1,4 +1,5 @@
 import atexit
+import asyncio
 from os import path
 from datetime import datetime
 
@@ -12,6 +13,7 @@ PLUGIN_NAME = 'message_dump'
 
 _bot = get_bot()
 _data_frame: DataFrame = None
+_lock = asyncio.Lock()
 
 
 @on_natural_language(only_to_me=False, only_short_message=False)
@@ -31,8 +33,12 @@ async def _(session: NLPSession):
         }, ignore_index=True)
 
     if len(_data_frame.index) >= _bot.config.MESSAGE_DUMP_SINGLE_FILE_MAX_ROWS:
-        await aio.run_sync_func(_data_frame.to_parquet, make_filename())
-        _data_frame = None
+        async with _lock:
+            # the following line may use I/O for a little bit time,
+            # but since we only do it every MESSAGE_DUMP_SINGLE_FILE_MAX_ROWS
+            # messages, it's ok to await here
+            await aio.run_sync_func(_data_frame.to_parquet, make_filename())
+            _data_frame = None
 
 
 @atexit.register
