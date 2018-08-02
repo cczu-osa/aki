1a3132e4691f572aa6e2bfc7dd5d4a65fdd31593
Richard Chien <richardchienthebest@gmail.com>
2018-08-02 23:27:48 +0800

    Replace requirements that don't support Python 3.7 yet

--- maruko/plugins/message_dump.py
+++ maruko/plugins/message_dump.py
@@ -42,14 +42,16 @@ async def append_message(data):
             # the following line may use I/O for a little bit time,
             # but since we only do it every MESSAGE_DUMP_SINGLE_FILE_MAX_ROWS
             # messages, it's ok to await here
-            await aio.run_sync_func(_data_frame.to_parquet, make_filename())
+            await aio.run_sync_func(_data_frame.to_parquet,
+                                    make_filename(),
+                                    compression='gzip')
             _data_frame = None
 
 
 @atexit.register
 def exit_callback():
     if _data_frame is not None:
-        _data_frame.to_parquet(make_filename())
+        _data_frame.to_parquet(make_filename(), compression='gzip')
 
 
 def make_filename() -> str:
