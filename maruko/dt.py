6eb455c918b993a6bc12a2806b4d3c3a794913b5
Richard Chien <richardchienthebest@gmail.com>
2018-09-02 09:50:49 +0800

    Improve message collector

---
+++ maruko/dt.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import Optional
+
+import pytz
+from pandas import Timestamp
+
+CST_TIMEZONE = 'Asia/Shanghai'
+
+
+def beijing_now(freq: Optional[str] = None) -> datetime:
+    now = datetime.now(pytz.timezone(CST_TIMEZONE))
+    if freq is not None:
+        now = Timestamp(now).round(freq)
+    return now
