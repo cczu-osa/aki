f512b95d3719a34eec92859c051d0635c7a3f983
Richard Chien <richardchienthebest@gmail.com>
2018-12-17 20:56:33 +0800

    Update

--- run.py
+++ run.py
@@ -1,41 +1,39 @@
 import os
 import sys
 
+import amadeus
 
-def main():
-    deployment_env = os.getenv('DEPLOYMENT_ENV', 'dev').lower()
+deployment_env = os.getenv('DEPLOYMENT_ENV', 'dev').lower()
 
-    config = None
-    if deployment_env in ('dev', 'development'):
-        try:
-            import config_dev as config
-        except ImportError:
-            pass
-    elif deployment_env in ('test', 'testing'):
-        try:
-            import config_test as config
-        except ImportError:
-            pass
-    elif deployment_env in ('prod', 'production'):
-        try:
-            import config_prod as config
-        except ImportError:
-            pass
+config = None
+if deployment_env in ('dev', 'development'):
+    try:
+        import config_dev as config
+    except ImportError:
+        pass
+elif deployment_env in ('test', 'testing'):
+    try:
+        import config_test as config
+    except ImportError:
+        pass
+elif deployment_env in ('prod', 'production'):
+    try:
+        import config_prod as config
+    except ImportError:
+        pass
 
-    if config is None:
-        try:
-            import config
-        except ImportError:
-            pass
+if config is None:
+    try:
+        import config
+    except ImportError:
+        pass
 
-    if config is None:
-        print('There is no configuration file!', file=sys.stderr)
-        exit(1)
+if config is None:
+    print('There is no configuration file!', file=sys.stderr)
+    exit(1)
 
-    import amadeus
-    bot = amadeus.init(config)
-    bot.run()
-
+bot = amadeus.init(config)
+app = bot.asgi
 
 if __name__ == '__main__':
-    main()
+    bot.run()
