cbe081ce62327382c52a0d81771fcd1de88d7e30
Richard Chien <richardchienthebest@gmail.com>
2018-12-19 17:28:35 +0800

    Add "exec" plugin

---
+++ amadeus/plugins/exec.py
@@ -0,0 +1,24 @@
+import asyncio
+import pprint
+from typing import Awaitable, Callable
+
+import none.permission as perm
+from none import on_command, CommandSession
+
+
+@on_command('exec', permission=perm.SUPERUSER)
+async def _(session: CommandSession):
+    code = session.current_arg
+    tmp_locals = {}
+    exec(code, None, tmp_locals)
+    await session.send(f'Locals:\n{pprint.pformat(tmp_locals, indent=2)}')
+
+    if isinstance(tmp_locals.get('run'), Callable):
+        try:
+            res = tmp_locals['run'](session.bot, session.ctx)
+            if isinstance(res, Awaitable):
+                res = await asyncio.wait_for(res, 6)
+            await session.send(f'执行成功\n'
+                               f'返回：\n{pprint.pformat(res, indent=2)}')
+        except Exception as e:
+            await session.send(f'执行失败\n异常：\n{pprint.pformat(e, indent=2)}')
