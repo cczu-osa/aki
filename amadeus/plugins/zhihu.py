b52239e2b5f8e2ca13aedeb4482af2247636b469
Richard Chien <richardchienthebest@gmail.com>
2018-12-28 01:02:43 +0800

    Add "zhihu" plugin

---
+++ amadeus/plugins/zhihu.py
@@ -0,0 +1,35 @@
+from typing import Optional, List, Dict, Any
+
+from nonebot import on_command, CommandSession
+
+from amadeus.aio import requests
+from amadeus.cache import cached
+
+DAILY_LATEST_API_URL = 'https://news-at.zhihu.com/api/4/news/latest'
+DAILY_STORY_URL = 'https://daily.zhihu.com/story/{id}'
+
+
+@cached(ttl=5 * 60)
+async def get_latest_news() -> Optional[List[Dict[str, Any]]]:
+    resp = await requests.get(DAILY_LATEST_API_URL, headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+    })
+    payload = await resp.json()
+    if not isinstance(payload, dict) or 'stories' not in payload:
+        return None
+    return payload.get('stories') or []
+
+
+@on_command(('zhihu', 'daily'), aliases=['知乎日报'])
+async def paste(session: CommandSession):
+    stories = await get_latest_news()
+    if stories is None:
+        session.finish('查询失败了……')
+    elif not stories:
+        session.finish('暂时还没有内容哦')
+
+    reply = ('今天的知乎日报内容如下：\n\n' +
+             '\n\n'.join(f'{story["title"]}\n'
+                         f'{DAILY_STORY_URL.format(id=story["id"])}'
+                         for story in stories))
+    session.finish(reply)
