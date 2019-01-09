c9cdb9970010febf1fe2a0014d82d9b97d5b0701
Richard Chien <richardchienthebest@gmail.com>
2019-01-09 12:48:49 +0800

    Improve repeater function

--- amadeus/plugins/repeater.py
+++ amadeus/plugins/repeater.py
@@ -1,27 +1,41 @@
 import random
-from collections import Counter
+from dataclasses import dataclass
+from typing import Dict
 
 import nonebot.permission as perm
 from nonebot import on_natural_language, NLPSession, NLPResult
-from nonebot.helpers import context_id
 
-counter = Counter()
-last_msgs = {}
+
+@dataclass
+class Record:
+    last_msg: str
+    last_user_id: int
+    repeat_count: int = 0
+
+
+records: Dict[int, Record] = {}
 
 
 @on_natural_language(only_to_me=False, permission=perm.GROUP)
 async def _(session: NLPSession):
-    ctx_id = context_id(session.ctx, mode='group')
+    group_id = session.ctx['group_id']
+    user_id = session.ctx['user_id']
+    msg = session.msg
 
-    if last_msgs.get(ctx_id) != session.msg:
-        counter[ctx_id] = 0
-        last_msgs[ctx_id] = session.msg
-    counter[ctx_id] += 1
+    record = records.get(group_id)
+    if record is None:
+        record = Record(msg, user_id, repeat_count=1)
+        records[group_id] = record
+        return
 
-    if counter[ctx_id] == 3:
+    if record.last_msg != msg or record.last_user_id == user_id:
+        return
+
+    record.last_user_id = user_id
+    record.repeat_count += 1
+    if record.repeat_count == 3:
         return NLPResult(
             random.randint(0, 1) * 90.0,
             ('delayed_echo',),
-            {'delay': random.randint(5, 20) / 10,
-             'message': session.msg}
+            {'delay': random.randint(5, 20) / 10, 'message': msg}
         )
