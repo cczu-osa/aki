7bc0899ff64e397e51b6697ccf24cccb56f26762
Richard Chien <richardchienthebest@gmail.com>
2019-01-09 14:51:50 +0800

    Fix bug

--- amadeus/plugins/repeater.py
+++ amadeus/plugins/repeater.py
@@ -4,6 +4,7 @@ from typing import Dict
 
 import nonebot.permission as perm
 from nonebot import on_natural_language, NLPSession, NLPResult
+from nonebot.helpers import context_id
 
 
 @dataclass
@@ -13,19 +14,19 @@ class Record:
     repeat_count: int = 0
 
 
-records: Dict[int, Record] = {}
+records: Dict[str, Record] = {}
 
 
 @on_natural_language(only_to_me=False, permission=perm.GROUP)
 async def _(session: NLPSession):
-    group_id = session.ctx['group_id']
+    group_ctx_id = context_id(session.ctx, mode='group')
     user_id = session.ctx['user_id']
     msg = session.msg
 
-    record = records.get(group_id)
+    record = records.get(group_ctx_id)
     if record is None:
         record = Record(msg, user_id, repeat_count=1)
-        records[group_id] = record
+        records[group_ctx_id] = record
         return
 
     if record.last_msg != msg or record.last_user_id == user_id:
