be5dee995a543126639c91c46f55463e2ef5ea92
Richard Chien <richardchienthebest@gmail.com>
2019-01-19 11:50:47 +0800

    Rearrange config_base

--- config_base.py
+++ config_base.py
@@ -8,24 +8,16 @@ COMMAND_SEP = {'/', '.'}
 SESSION_CANCEL_EXPRESSION = ('好的',)
 SESSION_RUN_TIMEOUT = timedelta(seconds=20)
 
+# 数据文件夹
 DATA_FOLDER = path.join(path.dirname(__file__), 'data')
 
+# 数据库 URL
 DATABASE_URL = ''
 
-BAIDU_AIP_APP_ID = ''
-BAIDU_AIP_API_KEY = ''
-BAIDU_AIP_SECRET_KEY = ''
-
-LTP_CLOUD_API_KEY = ''
-
-JOKE_API_KEY = ''  # 输入你的api_key,来源：聚合数据服务——笑话大全
-
-DICTIONARY_API_KEY = ''  # 输入你的api_key，来源：聚合数据服务——成语字典
-
-TULING123_API_KEY = ''
-
+# 消息采集器 dump 频率
 MESSAGE_COLLECTOR_DUMP_FREQ = 'H'
 
+# aiocache 配置
 AIOCACHE_DEFAULT_CONFIG = {
     'cache': 'aiocache.SimpleMemoryCache',
     'serializer': {
@@ -33,4 +25,20 @@ AIOCACHE_DEFAULT_CONFIG = {
     }
 }
 
+# 使用手册图片地址
 MANUAL_IMAGE_URL_FORMAT = 'https://raw.githubusercontent.com/cczu-osa/amadeus/master/manual/screenshots/{}.png'
+
+# 百度 AIP
+BAIDU_AIP_APP_ID = ''
+BAIDU_AIP_API_KEY = ''
+BAIDU_AIP_SECRET_KEY = ''
+
+# 语言云
+LTP_CLOUD_API_KEY = ''
+
+# 图灵机器人
+TULING123_API_KEY = ''
+
+# 聚合数据
+JUHE_JOKE_API_KEY = ''  # 笑话大全
+JUHE_IDIOM_API_KEY = ''  # 成语词典
