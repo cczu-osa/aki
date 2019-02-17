51862a20c11e1ab69aa09b5a7d4eb880e894ddee
Richard Chien <richardchienthebest@gmail.com>
2019-02-17 20:28:57 +0800

    修复 ack 的 bug

--- aki/plugins/greeting.py
+++ aki/plugins/greeting.py
@@ -1,6 +1,6 @@
 import random
 
-from nonebot import on_natural_language, NLPSession, NLPResult
+from nonebot import on_natural_language, NLPSession, IntentCommand
 from nonebot import permission as perm
 from nonebot.helpers import render_expression as expr
 
@@ -8,19 +8,20 @@ from nonebot.helpers import render_expression as expr
 @on_natural_language(keywords={'打个招呼', '打招呼'}, permission=perm.SUPERUSER)
 async def _(session: NLPSession):
     if '大家' in session.msg_text:
-        return NLPResult(100.0, 'echo', {'message': '大家好～'})
+        return IntentCommand(100.0, 'echo', args={'message': '大家好～'})
 
 
 @on_natural_language(allow_empty_message=True)
 async def _(session: NLPSession):
-    if not session.msg:
+    if not session.msg.strip():
         # empty message body
         nicks = list(session.bot.config.NICKNAME)
         if nicks:
             nick = nicks[0]
         else:
             nick = None
-        await session.send(expr(expr_ack, nick=nick))
+        return IntentCommand(100.0, 'echo',
+                             args={'message': expr(expr_ack, nick=nick)})
 
 
 def expr_ack(**kwargs):
